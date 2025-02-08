92be0a7de9ed38f1b504d5044c77e8af5027a027
Ruslan <kanii.ues@gmail.com>
2025-02-08 16:41:03 +0300

    add uppdate

--- js/script.js
+++ js/script.js
@@ -2,21 +2,30 @@ document.addEventListener("DOMContentLoaded", () => {
   const heart = document.getElementById("heart-container");
   const button = document.getElementById("love-button");
   const heartsContainer = document.getElementById("hearts-container");
+  const headerText = document.getElementById("header-text");
+  const quote = document.getElementById("quote");
   let clicked = false;
   let heartInterval;
 
   button.addEventListener("click", () => {
     if (!clicked) {
-      button.textContent = "Нажми еще раз";
+      button.textContent = "";
+      headerText.textContent = "Я тебя люблю!";
       heart.style.opacity = "0";
+      quote.style.opacity = "0";
       heart.style.transition = "opacity 0.5s";
+      quote.style.transition = "opacity 0.5s";
       heartInterval = setInterval(createFallingHeart, 300);
+      button.classList.add("glow");
       clicked = true;
     } else {
       button.textContent = "Нажми меня";
+      headerText.textContent = "С Днем Святого Валентина!";
       heart.style.opacity = "1";
+      quote.style.opacity = "1";
       clearInterval(heartInterval);
       heartsContainer.innerHTML = "";
+      button.classList.remove("glow");
       clicked = false;
     }
   });
